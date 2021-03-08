6fee602adb1c4ac6d3dfc1fc937e007accc67c62
yzhan112 <46976896+yzhan112@users.noreply.github.com>
2021-03-08 14:09:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def app():
     # Generate plot
 
     st.title("Fortune Companies DE&I Analysis")
-    st.write(f'Eric Yongliang Zhang')
+    st.write(f'Yongliang Zhang')
     
     st.markdown(""" 
 
